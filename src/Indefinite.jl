b6e733fd15b66f8e213848919b785ff7077d577b
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2023-05-04 08:52:10 +0200

    Some insertion.

--- src/Indefinite.jl
+++ src/Indefinite.jl
@@ -12,6 +12,7 @@ include("ExternalCalls.jl")
 function __init__()
     binpaths = [ @generate_wrappers(polyhedral_jll) ]
     ENV["PATH"] = join([binpaths...,ENV["PATH"]], ":")
+    GAP.Packages.load("gap")
 end
 
 end
